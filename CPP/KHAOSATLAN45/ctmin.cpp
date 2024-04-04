f19a1fcfe890a404dc3aae502f87bbef212bef71
Huy Nguyen <huythedev@gmail.com>
2024-04-04 21:14:46 +0700

    Update

--- CPP/KHAOSATLAN45/ctmin.cpp
+++ CPP/KHAOSATLAN45/ctmin.cpp
@@ -22,36 +22,36 @@ int main() {
     ll n, X;
     cin >> n >> X;
 
-    vector<int> a(n * 2);
-    for (int i = 0; i < n; ++i) {
+    vector<ll> a(n * 2);
+    for (ll i = 0; i < n; ++i) {
         cin >> a[i];
         a[i + n] = a[i];
         if (a[i] >= X) {
-            cout << 1;
+            cout << 0;
             return 0;
         }
     }
 
     ll sum = 0;
-    for (int i = 0; i < n; ++i)
+    for (ll i = 0; i < n; ++i)
         sum += a[i];
 
     if (sum < X) {
-        cout << -1;
+        cout << 0;
         return 0;
     }
 
     ll min_length = LLONG_MAX;
     ll tmp = 0;
-    int start = 0;
-    for (int i = 0; i < n * 2; ++i) {
+    ll start = 0;
+    for (ll i = 0; i < n * 2; ++i) {
         tmp += a[i];
         while (tmp - a[start] >= X) {
             tmp -= a[start];
             start++;
         }
         if (tmp >= X) {
-            min_length = min(min_length, (ll)(i - start + 1));
+            min_length = min(min_length, (i - start + 1));
         }
     }
 
