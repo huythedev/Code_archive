3e0861765968bb67d9ffbc060eda570d5e6193ae
Huy Nguyen <huythedev@gmail.com>
2024-04-16 16:24:39 +0700

    Update

--- CPP/LQDOJ/cses2413.cpp
+++ CPP/LQDOJ/cses2413.cpp
@@ -14,24 +14,25 @@ void docfile() {
     }
 }
 
-const int MOD = 1e9 + 7;
+const ll MOD = 1e9 + 7;
+const ll MAXN = 1e6 + 6;
+
+ll dp[MAXN][3];
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     docfile();
 
-    int t;
-    cin >> t;
-
-    int dp[(int)1e6 + 5][3];
+    ll t; cin >> t;
+    
     dp[1][1] = dp[1][2] = 1;
-    for (int i = 2; i < (int)1e6 + 5; i++) {
+    for (ll i = 2; i < MAXN; i++) {
         dp[i][1] = (dp[i-1][1] * 4 % MOD + dp[i-1][2]) % MOD;
         dp[i][2] = (dp[i-1][1] + dp[i-1][2] * 2 % MOD) % MOD;
     }
     while(t--) {
-        int n; cin >> n;
+        ll n; cin >> n;
         cout << (dp[n][1] + dp[n][2]) % MOD << ln;
     }
 
