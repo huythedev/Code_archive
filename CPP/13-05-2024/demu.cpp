20a0186c257037cd5647dcdd113362b19305d467
Huy Nguyen <huythedev@gmail.com>
2024-05-14 18:06:14 +0700

    update

---
+++ CPP/13-05-2024/demu.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int cntdiv(int n) {
+	int res = 0;
+	for(int i = 1; i * i <= n; ++i) {
+		if(n % i == 0) {
+			++res;
+			if(n / i != i)
+				++res;
+		}
+	}
+	return res;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n; cin >> n;
+	while(n--) {
+		int x; cin >> x;
+
+		cout << cntdiv(x) << endl;
+	}
+	return 0;
+}
