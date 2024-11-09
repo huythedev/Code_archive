ea1e0e51e657946b8865ba7dfc1fb1f9dc53197c
Huy Nguyen <huythedev@gmail.com>
2024-11-09 08:26:09 +0000

    Nov 9, 2024, 3:26 PM

--- CONTESTS/k23cwd20241109/cwdft.cpp
+++ CONTESTS/k23cwd20241109/cwdft.cpp
@@ -1,27 +1,58 @@
+// Author: Perry (https://perrythedev.com)
+// Problem Link: https://nbk.homes/problem/cwdft
 #include <bits/stdc++.h>
 using namespace std;
 
+#define NAME "cwdft"
+#define ln "\n"
+#define sz size()
+
+typedef long long ll;
+typedef long double ld;
+
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void docfile() {
+    if (ifstream(NAME ".INP")) {
+        freopen(NAME ".INP", "r", stdin);
+        freopen(NAME ".OUT", "w", stdout);
+    }
+}
+
+void time() {
+    cerr << ln << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s." 
+         << ln;
+}
+
 int main() {
-    long long N, M;
-    cin >> N >> M;
+    fastio();
+    docfile();
+
+    ll N, M; cin >> N >> M;
     
-    for (long long T = M; ; T++) {
+    for(ll T = M;; T++) {
         bool found = false;
-        for (long long a = 1; a * a <= T; a++) {
-            if (T % a == 0) {
-                long long b = T / a;
-                if (a <= N && b <= N) {
+        for(ll a = 1; a * a <= T; a++) {
+            if(T % a == 0) {
+                ll b = T / a;
+                if(a <= N && b <= N) {
                     found = true;
                     break;
                 }
             }
         }
-        if (found) {
-            cout << T << endl;
+        if(found) {
+            cout << T;
             return 0;
         }
     }
     
-    cout << -1 << endl;
+    cout << -1;
+
+    time();
     return 0;
 }
