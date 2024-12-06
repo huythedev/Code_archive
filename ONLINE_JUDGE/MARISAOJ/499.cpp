77f0f65382cb99454766a5b8b0d4dd0180fff636
Huy Nguyen <huythedev@gmail.com>
2024-12-06 12:47:22 +0000

    Dec 6, 2024, 7:47 PM

---
+++ ONLINE_JUDGE/MARISAOJ/499.cpp
@@ -0,0 +1,40 @@
+// Author: Perry (https://perrythedev.com)
+// Problem Link: https://marisaoj.com/problem/499
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NAME "NAME"
+#define ln "\n"
+#define sz size()
+
+typedef long long ll;
+typedef long double ld;
+
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void docfile() {
+    if (ifstream(NAME ".INP")) {
+        freopen(NAME ".INP", "r", stdin);
+        freopen(NAME ".OUT", "w", stdout);
+    }
+}
+
+void time() {
+    cerr << ln << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s." 
+         << ln;
+}
+
+int main() {
+    fastio();
+    docfile();
+
+    int l, r; cin >> l >> r;
+    for(; l <= r; l++) cout << l << " ";
+
+    time();
+    return 0;
+}
