7925d00d48839e2c9f00a928386a34791a47f966
Huy Nguyen <huythedev@gmail.com>
2024-12-17 00:59:18 +0000

    Dec 17, 2024, 7:59 AM

---
+++ DONT KNOW WHAT TO NAME THIS/romantoint.cpp
@@ -0,0 +1,65 @@
+// Author: Perry (https://perrythedev.com)
+// Problem Link: 
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NAME "NAME"
+#define ln "\n"
+#define sz size()
+
+typedef long long ll;
+typedef long double ld;
+
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void docfile() {
+    if (ifstream(NAME ".INP")) {
+        freopen(NAME ".INP", "r", stdin);
+        freopen(NAME ".OUT", "w", stdout);
+    }
+}
+
+void time() {
+    cerr << ln << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s." 
+         << ln;
+}
+
+int romantoint(string s) {
+    unordered_map<char, int> roman = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+    int n = s.sz;
+    int res = roman[s[n - 1]];
+
+    for(int i = n - 2; i >= 0; --i) {
+        if(roman[s[i]] < roman[s[i + 1]]) {
+            res -= roman[s[i]];
+        } else {
+            res += roman[s[i]];
+        }
+    }
+
+    return res;
+}
+
+int main() {
+    fastio();
+    docfile();
+
+    string s; cin >> s;
+    cout << romantoint(s) << ln;
+
+    time();
+    return 0;
+}
