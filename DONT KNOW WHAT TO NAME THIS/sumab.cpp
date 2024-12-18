a2c065cb91cfc057395352f7d2543d4f105dfd53
Huy Nguyen <huythedev@gmail.com>
2024-12-18 13:57:36 +0000

    Dec 18, 2024, 8:57 PM

---
+++ DONT KNOW WHAT TO NAME THIS/sumab.cpp
@@ -0,0 +1,73 @@
+// Author: Perry (https://perrythedev.com)
+// Problem Link: 
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NAME "NAME"
+#define ln "\n"
+#define sz size()
+#define skibidi void
+#define rizz int
+#define cap bool
+#define cooked auto
+#define flex for
+#define bussin return
+#define edging if
+#define amogus else
+#define goon while
+#define bruh break
+#define grind continue
+#define chad float
+#define gigachad double
+#define yap char
+#define grimace const
+#define sigma_rule case
+#define based default
+#define mewing do
+#define gyatt enum
+#define whopper extern
+#define cringe goto
+#define giga long
+#define edgy register
+#define soy short
+#define nut signed
+#define maxxing sizeof
+#define salty static
+#define gang struct
+#define ohio switch
+#define chungus union
+#define nonut unsigned
+#define schizo volatile
+#define yapping cout
+
+typedef long long ll;
+typedef long double ld;
+
+skibidi fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+skibidi docfile() {
+    if (ifstream(NAME ".INP")) {
+        freopen(NAME ".INP", "r", stdin);
+        freopen(NAME ".OUT", "w", stdout);
+    }
+}
+
+skibidi time() {
+    cerr << ln << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s." 
+         << ln;
+}
+
+rizz main() {
+    fastio();
+    docfile();
+
+    rizz a, b; cin >> a >> b;
+    yapping << a + b;
+
+    time();
+    return 0;
+}
