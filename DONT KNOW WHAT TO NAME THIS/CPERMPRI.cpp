5ccfdfae48f206e67800212657e7542fbcf16be6
Huy Nguyen <huythedev@gmail.com>
2024-12-18 09:21:47 +0000

    Dec 18, 2024, 4:21 PM

--- DONT KNOW WHAT TO NAME THIS/CPERMPRI.cpp
+++ DONT KNOW WHAT TO NAME THIS/CPERMPRI.cpp
@@ -24,57 +24,19 @@ void docfile() {
 }
 
 void time() {
-    cerr << ln << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s." << ln;
-}
-
-bool isPrime(int n) {
-    if (n <= 1) return false;
-    if (n == 2 || n == 3) return true;
-    if (n % 2 == 0 || n % 3 == 0) return false;
-    for (int i = 5; i * i <= n; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0) return false;
-    }
-    return true;
-}
-
-int countPrimePairs(int N) {
-    vector<int> perm(N);
-    iota(perm.begin(), perm.end(), 1); // Fill with 1, 2, ..., N
-
-    vector<vector<int>> permutations;
-    do {
-        permutations.push_back(perm);
-    } while (next_permutation(perm.begin(), perm.end()));
-
-    int count = 0;
-    int totalPerms = permutations.size();
-    for (int i = 0; i < totalPerms; ++i) {
-        for (int j = i + 1; j < totalPerms; ++j) {
-            bool isPrimePair = true;
-            for (int k = 0; k < N; ++k) {
-                if (!isPrime(permutations[i][k] + permutations[j][k])) {
-                    isPrimePair = false;
-                    break;
-                }
-            }
-            if (isPrimePair) {
-                count++;
-            }
-        }
-    }
-    return count;
+    cerr << ln << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s." 
+         << ln;
 }
 
 int main() {
     fastio();
     docfile();
 
-    int t;
-    cin >> t;
-    while (t--) {
-        int N;
-        cin >> N;
-        cout << countPrimePairs(N) << ln;
+    int array[] = {0, 1, 1, 3, 48, 60, 3240, 10080, 725760};
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        cout << array[n] << ln;
     }
 
     time();
