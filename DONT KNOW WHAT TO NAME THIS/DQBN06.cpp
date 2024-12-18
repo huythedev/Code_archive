a17fa3621b98d3c020963d61b7f90adebd94c2bf
Huy Nguyen <huythedev@gmail.com>
2024-12-18 08:58:48 +0000

    Dec 18, 2024, 3:58 PM

--- DONT KNOW WHAT TO NAME THIS/DQBN06.cpp
+++ DONT KNOW WHAT TO NAME THIS/DQBN06.cpp
@@ -24,20 +24,59 @@ void docfile() {
 }
 
 void time() {
-    cerr << ln << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s." 
-         << ln;
+    cerr << ln << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s." << ln;
 }
 
 int main() {
     fastio();
     docfile();
 
-    int n, k; cin >> n >> k;
+    int n, k;
+    cin >> n >> k;
     vector<int> a(n);
-    for(int &i : a)
+    for (int &i : a)
         cin >> i;
 
-    
+    // DP table to store the maximum number of elements that can be selected with a given sum
+    vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
+    vector<vector<bool>> take(n + 1, vector<bool>(k + 1, false));
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j <= k; ++j) {
+            // Do not take the current element
+            dp[i][j] = dp[i - 1][j];
+            // Take the current element if it does not exceed the sum
+            if (j >= a[i - 1] && dp[i - 1][j - a[i - 1]] + 1 > dp[i][j]) {
+                dp[i][j] = dp[i - 1][j - a[i - 1]] + 1;
+                take[i][j] = true;
+            }
+        }
+    }
+
+    // Find the maximum number of elements that can be selected
+    int max_elements = 0, sum = 0;
+    for (int j = 0; j <= k; ++j) {
+        if (dp[n][j] > max_elements) {
+            max_elements = dp[n][j];
+            sum = j;
+        }
+    }
+
+    // Reconstruct the solution
+    vector<int> res;
+    for (int i = n; i > 0; --i) {
+        if (take[i][sum]) {
+            res.push_back(i);
+            sum -= a[i - 1];
+        }
+    }
+
+    // Output the result
+    cout << max_elements << ln;
+    for (int i = res.size() - 1; i >= 0; --i) {
+        cout << res[i] << " ";
+    }
+    cout << ln;
 
     time();
     return 0;
