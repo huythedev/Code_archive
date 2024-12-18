2c9e76d50887e9324c57d64dadbed84830866f4c
Huy Nguyen <huythedev@gmail.com>
2024-12-18 23:56:42 +0000

    Dec 19, 2024, 6:56 AM

---
+++ ONLINE_JUDGE/MARISAOJ/45.cpp
@@ -0,0 +1,100 @@
+// Author: Perry (https://perrythedev.com)
+// Problem Link: https://marisaoj.com/problems/45
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NAME "NAME"
+#define ln "\n"
+#define sz size()
+#define skibidi void
+#define rizz int
+#define superrizz long long
+#define cap bool
+#define cooked auto
+#define flex for
+#define bussin return
+#define edging if
+#define amogus else
+#define goon while
+#define bruh break
+#define grind continue
+#define chad float
+#define gigachad double
+#define yap char
+#define grimace const
+#define sigma_rule case
+#define based default
+#define mewing do
+#define gyatt enum
+#define whopper extern
+#define cringe goto
+#define giga long
+#define edgy register
+#define soy short
+#define nut signed
+#define maxxing sizeof
+#define salty static
+#define gang struct
+#define ohio switch
+#define chungus union
+#define nonut unsigned
+#define schizo volatile
+#define yapping cout
+#define gegagedigedagedago cin
+#define dopdop true
+#define sus false
+
+typedef long long ll;
+typedef long double ld;
+
+skibidi fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+skibidi docfile() {
+    if (ifstream(NAME ".INP")) {
+        freopen(NAME ".INP", "r", stdin);
+        freopen(NAME ".OUT", "w", stdout);
+    }
+}
+
+skibidi time() {
+    cerr << ln << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s." 
+         << ln;
+}
+
+rizz main() {
+    fastio();
+    docfile();
+
+    string s; getline(gegagedigedagedago, s);
+    edging(s.sz < 8) {
+        yapping << "WEAK";
+    }
+    amogus {
+        cap has_lowercase_char = false, has_uppercase_char = false, has_digit = false;
+        flex(rizz i = 0; i < s.sz; ++i) {
+            edging(islower(s[i])) {
+                has_lowercase_char = true;
+            }
+            amogus edging(isupper(s[i])) {
+                has_uppercase_char = true;
+            }
+            amogus edging(isdigit(s[i])) {
+                has_digit = true;
+            }
+        }
+    }
+
+    edging(has_lowercase_char && has_uppercase_char && has_digit) {
+        yapping << "STRONG";
+    }
+    amogus {
+        yapping << "WEAK";
+    }
+
+    time();
+    return 0;
+}
