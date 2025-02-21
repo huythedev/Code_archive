6d8ef5a4242123336774081f79d9915bbceab932
Huy Nguyen <huythedev@gmail.com>
2025-02-21 00:37:20 +0000

    Feb 21, 2025, 7:37 AM

---
+++ CONTESTS/kslan32025/BAI1.cpp
@@ -0,0 +1,38 @@
+// Author: Perry (https://perrythedev.com)
+// Problem Link: https://nbk.homes/problem/bai1ks3
+// davul
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NAME "BAI1"
+#define ln "\n"
+#define sz size()
+
+typedef long long ll;
+typedef long double ld;
+
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void docfile() {
+    if (ifstream(NAME ".INP")) {
+        freopen(NAME ".INP", "r", stdin);
+        freopen(NAME ".OUT", "w", stdout);
+    }
+}
+
+void time() {
+    cerr << ln << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s." 
+         << ln;
+}
+
+int main() {
+    fastio();
+    docfile();
+
+    time();
+    return 0;
+}
