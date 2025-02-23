5f1d8a54fa54995f32b40bba049a969b86dea3df
Huy Nguyen <huythedev@gmail.com>
2025-02-23 07:27:51 +0000

    Feb 23, 2025, 2:27 PM

---
+++ ONLINE_JUDGE/HUSC/olpicpc078c17e.cpp
@@ -0,0 +1,38 @@
+// Author: Perry (https://perrythedev.com)
+// Problem Link: https://coder.husc.edu.vn/problem/olpicpc078c17e
+// davul
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NAME "olpicpc078c17e"
+#define ln "\n"
+#define sz size()
+
+typedef long long ll;
+typedef long double ld;
+
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void docfile() {
+    if (ifstream(NAME ".INP")) {
+        freopen(NAME ".INP", "r", stdin);
+        freopen(NAME ".OUT", "w", stdout);
+    }
+}
+
+void time() {
+    cerr << ln << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s." 
+         << ln;
+}
+
+int main() {
+    fastio();
+    docfile();
+
+    time();
+    return 0;
+}
