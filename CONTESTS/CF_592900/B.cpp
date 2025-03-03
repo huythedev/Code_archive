37a5bf50945c40a44adac256d6bef670959dce18
Huy Nguyen <huythedev@gmail.com>
2025-03-03 10:56:14 +0000

    Mar 3, 2025, 5:56 PM

---
+++ CONTESTS/CF_592900/B.cpp
@@ -0,0 +1,65 @@
+// Author: Perry (https://perrythedev.com)
+// Problem Link: https://codeforces.com/group/G0iFI97YZN/contest/592900/problem/B
+// davul
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NAME "NAME"
+#define ln "\n"
+#define sz size()
+
+typedef long long ll;
+typedef long double ld;
+
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void docfile() {
+    if (ifstream(NAME ".INP")) {
+        freopen(NAME ".INP", "r", stdin);
+        freopen(NAME ".OUT", "w", stdout);
+    }
+}
+
+void time() {
+    cerr << ln << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s." 
+         << ln;
+}
+
+ll factorial(ll n) {
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
+}
+
+ll f(ll x) {
+    if (x <= 9)
+        return factorial(x);
+    else {
+        return factorial(x % 10) + f(x / 10);
+    }
+}
+
+int main() {
+    fastio();
+    docfile();
+
+    int y; cin >> y;
+
+    ll x = 0, curr = 0;
+    while (curr < y) {
+        curr = f(x);
+        x++;
+        if (curr > y) {
+            x--;
+            break;
+        }
+    }
+
+    cout << x << ln;
+
+    time();
+    return 0;
+}
