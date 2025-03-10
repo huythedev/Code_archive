296505d68dd527ac89a367f1dd240b377b6ffb04
Huy Nguyen <huythedev@gmail.com>
2025-03-10 04:51:42 +0000

    🚀 Add dynamic programming solution to maximize points based on character sequence

--- CONTESTS/CF_594115/E.cpp
+++ CONTESTS/CF_594115/E.cpp
@@ -36,6 +36,33 @@ signed main() {
     docfile();
 
     cin >> st;
+    
+    int n = st.length();
+    vector<vector<int>> dp(n + 1, vector<int>(4, INT_MIN));
+    dp[0][0] = 0;  
+    
+    for (int i = 1; i <= n; ++i) {
+        char curr = st[i - 1];
+        
+        for (int j = 0; j <= 3; ++j) {
+            dp[i][j] = max(dp[i][j], dp[i-1][j]);
+        }
+        
+        if (curr == 'W') {
+            dp[i][1] = max(dp[i][1], dp[i-1][0] + 2);
+            dp[i][2] = max(dp[i][2], dp[i-1][1] + 2);
+            dp[i][3] = max(dp[i][3], dp[i-1][2] + 3);
+            dp[i][3] = max(dp[i][3], dp[i-1][3] + 3);
+        } 
+        else {
+            int points = (curr == 'D') ? 1 : 0;
+            for (int j = 0; j <= 3; ++j) {
+                dp[i][0] = max(dp[i][0], dp[i-1][j] + points);
+            }
+        }
+    }
+    
+    cout << max({dp[n][0], dp[n][1], dp[n][2], dp[n][3]}) << ln;
 
     time();
     return 0;
