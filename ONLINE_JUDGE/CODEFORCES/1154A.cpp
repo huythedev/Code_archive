69d2f1f8da3348e7318e85235480fcca02a3566b
Huy Nguyen <huythedev@gmail.com>
2025-03-13 03:05:15 +0000

    🧮 Add logic to read input, sort array, and calculate differences for output

--- ONLINE_JUDGE/CODEFORCES/1154A.cpp
+++ ONLINE_JUDGE/CODEFORCES/1154A.cpp
@@ -33,6 +33,16 @@ signed main() {
     fastio();
     docfile();
 
+    vector<int> a(4);
+    for (int i = 0; i < 4; ++i) {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    int x = a[3] - a[0];
+    int y = a[3] - a[1];
+    int z = a[3] - a[2];
+    cout << x << " " << y << " " << z << ln;
+
     time();
     return 0;
 }
