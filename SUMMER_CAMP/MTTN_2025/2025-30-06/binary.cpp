b4f9e939433ca680f9eed860fef2e91685163394
Huy Nguyen <huythedev@gmail.com>
2025-07-01 16:26:10 +0700

    sus

--- SUMMER_CAMP/MTTN_2025/2025-30-06/binary.cpp
+++ SUMMER_CAMP/MTTN_2025/2025-30-06/binary.cpp
@@ -28,52 +28,37 @@ void time() {
          << ln;
 }
 
-bool cnt(ll n, int k) {
-    if (k == 0) 
-        return true;
-
-    if (n == 0 && k > 0) 
-        return false;
-
-    int res = 0;
-    for (int i = 0; i < 64; ++i) {
-        if ((n >> i) & 1)
-            res++;
-        else 
-            res = 0;
-        
-        if (res >= k)
-            return true;
-    }
-    return false;
-}
-
 void solve() {
-    ll n, res = LLONG_MAX;
-    int k; cin >> n >> k;
-
-    if (cnt(n, k)) {
-        cout << n << ln;
-        return;
-    }
-
-    for (int i = 0; i <= 63 - k; ++i) {
-        ll mask = ((1LL << k) - 1) << i;
-        ll tmp = (i + k >= 63) ? 0 : ~((1LL << (i + k)) - 1);
-        ll tmp2 = n & tmp;
-        ll candidate1 = tmp2 | mask;
-
-        if (candidate1 >= n)
-            res = min(res, candidate1);
-        else {
-            if (i + k < 64) {
-                ll candidate2 = (tmp2 + (1LL << (i + k))) | mask;
-                res = min(res, candidate2);
+    unsigned long long n, k; cin >> n >> k;
+    bitset<63> bs(n);
+ 
+    int bits_1 = 0;
+    for (unsigned int mask = 0; mask < 64; ++mask) {
+        if (bs[mask] == 1) {
+            ++bits_1;
+            if (bits_1 >= k) {
+                cout << n << ln;
+                return;
             }
         }
+        else bits_1 = 0;
     }
-
-    cout << res << ln;
+ 
+    for (unsigned int mask = 0; mask < k; ++mask) {
+        bs[mask] = 1;
+    }
+ 
+    int idx = k, idx2 = 0;
+    while (bs[idx] == 1) {
+        bs[idx2] = 0;
+        if (bs.to_ullong() < n) {
+            bs[idx] = 1;
+            break;
+        }
+        ++idx; ++idx2;
+    }
+ 
+    cout << bs.to_ullong() << ln;
 }
 
 signed main() {
