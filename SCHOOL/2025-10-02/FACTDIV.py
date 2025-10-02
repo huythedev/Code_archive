2a99ff8422985bf9beec9738f5b66097a8538c6e
Huy Nguyen <huythedev@gmail.com>
2025-10-02 06:30:39 +0000

    Auto commit

--- SCHOOL/2025-10-02/FACTDIV.py
+++ SCHOOL/2025-10-02/FACTDIV.py
@@ -3,3 +3,10 @@ import math
 with open("FACTDIV.INP", "r") as stdin:
     n, m = map(int, stdin.read().split())
 
+nFactorial = math.factorial(n)
+result = nFactorial // m
+
+result %= int(1e9 + 7)
+
+with open("FACTDIV.OUT", "w") as stdout:
+    stdout.write(str(int(result)) + "\n")
