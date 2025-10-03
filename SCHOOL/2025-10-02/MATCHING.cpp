d6f2d18fcf0f23677533375139d41b075f330c52
Huy Nguyen <huythedev@gmail.com>
2025-10-03 00:15:27 +0000

    Auto commit

--- SCHOOL/2025-10-02/MATCHING.cpp
+++ SCHOOL/2025-10-02/MATCHING.cpp
@@ -29,7 +29,75 @@ void time() {
 }
 
 void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> S(n + 1), A(m + 1);
+    for (int i = 1; i <= n; ++i) cin >> S[i];
+    for (int i = 1; i <= m; ++i) cin >> A[i];
 
+    vector<int> L(n + 1, 0), R(n + 1, 0);
+    vector<int> posVal(n + 1, 0);
+    set<int> seen;
+
+    for (int i = 1; i <= n; ++i) {
+        auto itPred = seen.lower_bound(S[i]);
+        if (itPred != seen.begin()) {
+            --itPred;
+            L[i] = posVal[*itPred];
+        }
+        auto itSucc = seen.upper_bound(S[i]);
+        if (itSucc != seen.end()) {
+            R[i] = posVal[*itSucc];
+        }
+        seen.insert(S[i]);
+        posVal[S[i]] = i;
+    }
+
+    auto ok = [&](const vector<int>& T, int i, int q) -> bool {
+        int base = i - q - 1;
+        
+        int li = L[q + 1];
+        if (li && !(T[base + li] < T[i])) 
+            return false;
+        
+        int ri = R[q + 1];
+        if (ri && !(T[i] < T[base + ri])) 
+            return false;
+        
+        return true;
+    };
+
+    vector<int> pi(n + 1, 0);
+    for (int i = 2; i <= n; ++i) {
+        int q = pi[i - 1];
+
+        while (q > 0 && !ok(S, i, q)) 
+            q = pi[q];
+
+        if (ok(S, i, q)) 
+            ++q;
+        
+        pi[i] = q;
+    }
+
+    vector<int> ans;
+    int q = 0;
+    for (int i = 1; i <= m; ++i) {
+        while (q > 0 && !ok(A, i, q)) 
+            q = pi[q];
+        
+        if (ok(A, i, q)) 
+            ++q;
+        
+        if (q == n) {
+            ans.push_back(i - n + 1); 
+            q = pi[q];
+        }
+    }
+
+    cout << (int)ans.size() << ln;
+    for (int &x : ans) cout << x << ' ';
+    cout << ln;
 }
 
 signed main() {
